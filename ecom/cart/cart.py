a5b03f902a9ab1fda5afe4d7116aae4355137e04
Brandon Collins <bpcollins1995@gmail.com>
2024-02-10 17:21:42 +0000

    Add Products to Cart

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,3 +1,4 @@
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -6,8 +7,18 @@ class Cart():
         cart = self.session.get('session_key')
 
         #If new user, create session!
-        if 'session_key' is not in request.session:
+        if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
 
         #Ensure cart is available on all pages
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     """ A view that renders the cart contents page """
     return render(request, "cart_summary.html")
 
-
-
 def cart_add(request):
-    pass 
+    cart = Cart(request)
+    #Test for Post
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+
+        #Grab product in database
+        product = get_object_or_404(Product, id=product_id)
+        #Save to session
+        cart.add(product=product)
+        #Return Response
+        response = JsonResponse({'Product Name: ' : product.name})
+        return response 
 
 def cart_delete(request):
     pass
