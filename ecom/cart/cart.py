9d78ec975ac3a915508aa92859158b36dd76ed2d
Brandon Collins <bpcollins1995@gmail.com>
2024-02-11 04:15:41 +0000

    Update Shopping Cart

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -38,3 +38,16 @@ class Cart():
     def get_quants(self):
         quantities = self.cart
         return quantities 
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        ourcart = self.cart
+        #Update Cart
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+
+        thing = self.cart
+        return thing
